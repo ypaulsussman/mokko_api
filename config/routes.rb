0727315b64fce5445bca246c3b1c6dcc8e39bc07
Y Sussman <ypaulsussman@gmail.com>
2021-04-24 11:18:41 -0500

    fix routes; add procfile

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,15 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :interrogations
-  resources :tags
-  resources :prompts
-  resources :notes
-  resources :decks
-  # For details on the DSL available within this file,
-  # see https://guides.rubyonrails.org/routing.html
-
   scope :api, defaults: { format: :json } do
     root to: 'application#lander'
     post 'login', to: 'tokens#create'
 
-    # resources :studies, only: [:index, :show]
-    # resources :study_searches, only: [:create] do
-    #   post 'autocomplete', on: :collection
-    # end
+    resources :interrogations
+    resources :tags
+    resources :prompts
+    resources :notes
+    resources :decks
+    # resources :users
   end
-
-  # resources :users
 end
