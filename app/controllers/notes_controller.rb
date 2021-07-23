10f00a740ae5523abac467df8e31436e26849281
Y Sussman <ypaulsussman@gmail.com>
2021-07-22 20:24:42 -0500

    add tag-CRUD to note detail-view

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -54,17 +54,49 @@ class NotesController < ApplicationController
   # GET /notes/1
   def show
     render json: { note: @note.as_json(include: [:deck, :tags, :mokkos]),
-                   selectable_decks: @current_user.decks.select(:id, :title) }
+                   selectable_decks: @current_user.decks.select(:id, :title),
+                   selectable_tags:
+                   Tag.where(id:
+                     NoteTag.where(note_id:
+                       Note.where(deck_id:
+                         @current_user.decks)).select(:tag_id))
+                      .select(:id, :content) }
   end
 
   # PATCH/PUT /notes/1
   def update
-    if @note.update(note_params)
-      render json: { note: @note.as_json(include: [:deck, :tags, :mokkos]),
-                     selectable_decks: @current_user.decks.select(:id, :title) }
-    else
-      render json: @note.errors, status: :unprocessable_entity
+    ActiveRecord::Base.transaction do
+      @note.update!(note_params)
+
+      if params[:tagsToRemove].length.positive?
+        remove_ids = params[:tagsToRemove].map do |tag|
+          tag['id']
+        end
+        NoteTag.destroy_by(note_id: @note, tag_id: remove_ids)
+      end
+
+      if params[:tagsToAdd].length.positive?
+        params[:tagsToAdd].map do |tag|
+          if tag['id'].nil?
+            new_tag = Tag.create!(content: tag['content'])
+            NoteTag.create!(note_id: @note.id, tag_id: new_tag.id)
+          else
+            NoteTag.create!(note_id: @note.id, tag_id: tag['id'])
+          end
+        end
+      end
     end
+
+    render json: { note: Note.find_by(id: @note).as_json(include: [:deck, :tags, :mokkos]),
+                   selectable_decks: @current_user.decks.select(:id, :title),
+                   selectable_tags:
+                    Tag.where(id:
+                      NoteTag.where(note_id:
+                        Note.where(deck_id:
+                          @current_user.decks)).select(:tag_id))
+                       .select(:id, :content) }
+  rescue StandardError => e
+    render json: { errors: e }, status: :unprocessable_entity
   end
 
   private
@@ -79,6 +111,6 @@ class NotesController < ApplicationController
   end
 
   def note_params
-    params.require(:note).permit(:deck_id, :active, :next_occurrence, :current_interval, :content)
+    params.permit(:id, :deck_id, :active, :next_occurrence, :current_interval, :content)
   end
 end
